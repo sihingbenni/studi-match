91627b09c9186e08b58eabe8731225255e654be0
sihingbenni <sihing.benni@gmx.de>
2023-09-22 15:24:11 +0200

    MOD logging in demo app

--- lib/widgets/demo/my_demo_app.dart
+++ lib/widgets/demo/my_demo_app.dart
@@ -75,6 +75,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    logger.d('build()');
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
