f4eda7801183665af770fee533ab26f26f4017b9
sihingbenni <sihing.benni@gmx.de>
2023-10-01 22:52:40 +0200

    ADD documentation to JobProfessionsProvider
    Took 4 minutes

--- lib/providers/job_professions_provider.dart
+++ lib/providers/job_professions_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:studi_match/models/query_parameters.dart';
 import 'package:studi_match/services/employment_agency/job_search_service.dart';
 
+/// Provider for the list of existing job professions.
 class JobProfessionsProvider extends ChangeNotifier {
   final _queryParameters = QueryParameters();
   final _service = EAJobSearchService();
@@ -10,12 +11,16 @@ class JobProfessionsProvider extends ChangeNotifier {
 
   List<String> get fieldOfWorks => _fieldOfWorks;
 
+  /// Construct the JobProfessionsProvider and start getting the list of professions.
+  /// Configure the query parameters to only get the fieldOfWorks facet.
   JobProfessionsProvider() {
     _queryParameters.size = 1;
     _queryParameters.profession = 'Non existing field of Work';
     getProfessions();
   }
 
+  /// Sends a simplified request to the job search api endpoint to get a list of all professions.
+  /// This list contains only the fieldOfWorks facet.
   void getProfessions() => _service.callJobsApi(_queryParameters).then((response) {
         if (response.facets.fieldOfWork != null) {
           _fieldOfWorks = response.facets.fieldOfWork!.counts.keys.toList();
