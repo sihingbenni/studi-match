396a23daabaae15042a93563a0ed4340540f6e13
Jane <janeluciaschoenfeld@gmail.com>
2023-10-09 18:11:10 +0200

    add multidexapplication

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:studi_match/screens/home/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   // runApp(const MyDemoApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const StudiMatchApp());
 }
 
