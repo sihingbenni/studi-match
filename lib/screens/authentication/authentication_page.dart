1eafbce4beb152c0f5c467f0482e5736fa55002c
Jane <janeluciaschoenfeld@gmail.com>
2023-10-14 13:06:49 +0200

    styling authetication page as text wrap did not work otherwise

--- lib/screens/authentication/authentication_page.dart
+++ lib/screens/authentication/authentication_page.dart
@@ -24,100 +24,104 @@ class AuthenticationPage extends StatelessWidget {
             },
           ),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    body: ListView(
+      padding: const EdgeInsets.all(16.0),
+      children: [
+        SizedBox(
+          // TODO: better way to set height
+          height:  MediaQuery.of(context).size.height * 0.85,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   SvgPicture.asset(
-                    'assets/images/undraw_mobile_encryption.svg',
-                    width: 300,
-                    height: 300,
+                  'assets/images/undraw_mobile_encryption.svg',
+                  width: 300,
+                  height: 300,
+                ),
+                const Text(
+                  'Hallo 👋',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+                ),
+                const Text('und willkommen zu Studi Match. Logge dich ein oder erstelle einen Account, um passende Angebote zu finden und deine Zukunft zu gestalten!',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
-                  const Text(
-                    'Hallo 👋',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
-                  ),
-                  SizedBox(
-                    child: Column(
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.greenAccent,
-                            ),
-                            child: const Text(
-                              'Login',
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            )),
-                        ElevatedButton(
-                            onPressed: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) =>
-                                      const SignUp(),
-                                ),
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                                side: const BorderSide(
-                              color: Colors.greenAccent,
-                              width: 2,
-                            )),
-                            child: const Text('Sign Up',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.greenAccent))),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Oder fahre mit den folgenden Möglichkeiten fort:',
-                          style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.greenAccent,
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
-                        SignInButton.mini(
-                            buttonType: ButtonType.google, onPressed: () {}),
-                        TextButton(
-                            onPressed: () {
-                              try {
-                                FirebaseAuth.instance
-                                    .signInAnonymously()
-                                    .then((value) {
-                                  print('Du bist nun authentifiziert.');
-                                  Navigator.of(context).pop();
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) =>
-                                        const OnboardingDialog(),
-                                  ));
-                                });
-                                // TODO: Loading indicator and pop up upon successfull login
-                              } on Exception catch (_) {
-                                throw Exception(const Text(
-                                    'Anonymer Login hat nicht funktioniert.'));
-                              }
-                            },
-                            child: const Text(
-                              'bleib anonym 🥷',
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 16),
-                            )),
-                      ],
+                      ),
                     ),
-                  )
-                ],
-              ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const SignUp(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        side: const BorderSide(
+                          color: Colors.greenAccent,
+                          width: 2,
+                        ),
+                      ),
+                      child: const Text('Sign Up', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.greenAccent)),
+                    ),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text(
+                      'Oder fahre mit den folgenden Möglichkeiten fort:',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SignInButton.mini(buttonType: ButtonType.google, onPressed: () {}),
+                    TextButton(
+                      onPressed: () {
+                        try {
+                          FirebaseAuth.instance.signInAnonymously().then((value) {
+                            print('Du bist nun authentifiziert.');
+                            Navigator.of(context).pop();
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const OnboardingDialog(),
+                              ),
+                            );
+                          });
+                          // TODO: Loading indicator and pop up upon successful login
+                        } on Exception catch (_) {
+                          throw Exception(const Text('Anonymer Login hat nicht funktioniert.'));
+                        }
+                      },
+                      child: const Text(
+                        'bleib anonym 🥷',
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
         ),
-      );
+      ],
+    ),
+  );
 }
