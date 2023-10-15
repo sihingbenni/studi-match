6b34c3d7e6bfe375201e59aac6ab6dbb68df306c
Jane <janeluciaschoenfeld@gmail.com>
2023-10-15 10:05:58 +0200

    redirect if logged in to job list

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,93 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:studi_match/screens/authentication/authentication_page.dart';
+import 'package:studi_match/screens/employment_agency/jobs_list.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        body: Stack(children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              image: NetworkImage(
-                  'https://images.unsplash.com/photo-1545315003-c5ad6226c272'),
-              fit: BoxFit.cover,
-            )),
-          ),
-          const Align(
-            alignment: Alignment.topCenter,
-            child: SizedBox(
-              height: 300,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.add_home_work_outlined,
-                    color: Colors.white,
-                    size: 80,
-                  ),
-                  Text(
-                    'Studi Match',
-                    style: TextStyle(
-                        fontSize: 36,
-                        letterSpacing: 2.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ],
+  Widget build(BuildContext context) => StreamBuilder<User?>(
+    stream: FirebaseAuth.instance.authStateChanges(),
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        return const EAJobsListScreen();
+      } else { return Scaffold(
+            body: Stack(children: [
+              Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1545315003-c5ad6226c272'),
+                  fit: BoxFit.cover,
+                )),
               ),
-            ),
-          ),
-          Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: 300,
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+              const Align(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  height: 300,
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const Text(
-                        'Match dein nächstes Abenteuer.',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
+                      Icon(
+                        Icons.add_home_work_outlined,
+                        color: Colors.white,
+                        size: 80,
                       ),
-                      ElevatedButton(
-                          onPressed: () => {
-                                Navigator.of(context).pop(),
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        const AuthenticationPage(),
-                                  ),
-                                )
-                              },
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.greenAccent,
-                              minimumSize: const Size(double.infinity, 50),
-                          ),
-                          child: const Text(
-                            'Jetzt durchstarten 🚀',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          )),
-                      const Text(
-                        'und finde dein neues Team hier!',
+                      Text(
+                        'Studi Match',
                         style: TextStyle(
-                            fontSize: 20,
+                            fontSize: 36,
+                            letterSpacing: 2.0,
                             fontWeight: FontWeight.bold,
                             color: Colors.white),
                       ),
                     ],
                   ),
                 ),
-              ))
-        ]),
-      );
+              ),
+              Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    height: 300,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Text(
+                            'Match dein nächstes Abenteuer.',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          ElevatedButton(
+                              onPressed: () => {
+                                    Navigator.of(context).pop(),
+                                    Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const AuthenticationPage(),
+                                      ),
+                                    )
+                                  },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.greenAccent,
+                                  minimumSize: const Size(double.infinity, 50),
+                              ),
+                              child: const Text(
+                                'Jetzt durchstarten 🚀',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold),
+                              )),
+                          const Text(
+                            'und finde dein neues Team hier!',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ))
+            ]),
+          );
+    }
+  }
+  );
 }
