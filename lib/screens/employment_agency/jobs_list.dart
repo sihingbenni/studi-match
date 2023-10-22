098f442e58a128fd82648288acd3398ec23ca083
Jane <janeluciaschoenfeld@gmail.com>
2023-10-22 17:38:34 +0200

    create widget for default appbar

--- lib/screens/employment_agency/jobs_list.dart
+++ lib/screens/employment_agency/jobs_list.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:studi_match/screens/account/account.dart';
-import 'package:studi_match/screens/authentication/authentication_page.dart';
+import 'package:studi_match/widgets/appbar/default_appbar.dart';
 import 'package:studi_match/widgets/lists/swipe_list.dart';
 
 import '../../widgets/router/nav_router.dart';
@@ -11,69 +9,7 @@ class EAJobsListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          leading: StreamBuilder<User?>(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.data?.isAnonymous ?? true) {
-                return IconButton(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        NavRouter(
-                          builder: (context) => const AuthenticationPage(),
-                        ),
-                      );
-                    },
-                    icon: const Icon(Icons.login));
-              } else if (snapshot.connectionState == ConnectionState.active) {
-                if (snapshot.hasData) {
-                  // User is logged in
-                  return FilledButton.tonal(
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        NavRouter(
-                          builder: (context) => const AccountPage(),
-                        ),
-                      );
-                    },
-                    style: FilledButton.styleFrom(
-                      shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(2.0),
-                      backgroundColor: Colors.transparent,
-                    ),
-                    child: snapshot.data?.photoURL != null
-                        ? CircleAvatar(
-                            backgroundImage:
-                                NetworkImage(snapshot.data!.photoURL!),
-                          )
-                        : const Icon(Icons.person),
-                  );
-                } else {
-                  // User is not logged in
-                  return IconButton(
-                    icon: const Icon(Icons.login),
-                    onPressed: () {
-                      Navigator.of(context).push(
-                        NavRouter(
-                          builder: (context) => const AuthenticationPage(),
-                        ),
-                      );
-                    },
-                  );
-                }
-              } else {
-                // Return a loading indicator while the authentication state is loading
-                return const CircularProgressIndicator();
-              }
-            },
-          ),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.filter_list_rounded),
-              onPressed: () {},
-            ),
-          ],
-        ),
+        appBar: const DefaultAppbar(),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.pop(context);
---
+++ lib/widgets/appbar/default_appbar.dart
@@ -0,0 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../../screens/account/account.dart';
+import '../../screens/authentication/authentication_page.dart';
+import '../router/nav_router.dart';
+
+class DefaultAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const DefaultAppbar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) => AppBar(
+    leading: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.data?.isAnonymous ?? true) {
+          return IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  NavRouter(
+                    builder: (context) => const AuthenticationPage(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.login));
+        } else if (snapshot.connectionState == ConnectionState.active) {
+          if (snapshot.hasData) {
+            // User is logged in
+            return FilledButton.tonal(
+              onPressed: () {
+                Navigator.of(context).push(
+                  NavRouter(
+                    builder: (context) => const AccountPage(),
+                  ),
+                );
+              },
+              style: FilledButton.styleFrom(
+                shape: const CircleBorder(),
+                padding: const EdgeInsets.all(2.0),
+                backgroundColor: Colors.transparent,
+              ),
+              child: snapshot.data?.photoURL != null
+                  ? CircleAvatar(
+                backgroundImage:
+                NetworkImage(snapshot.data!.photoURL!),
+              )
+                  : const Icon(Icons.person),
+            );
+          } else {
+            // User is not logged in
+            return IconButton(
+              icon: const Icon(Icons.login),
+              onPressed: () {
+                Navigator.of(context).push(
+                  NavRouter(
+                    builder: (context) => const AuthenticationPage(),
+                  ),
+                );
+              },
+            );
+          }
+        } else {
+          // Return a loading indicator while the authentication state is loading
+          return const CircularProgressIndicator();
+        }
+      },
+    ),
+    actions: [
+      IconButton(
+        icon: const Icon(Icons.filter_list_rounded),
+        onPressed: () {},
+      ),
+    ],
+  );
+}
