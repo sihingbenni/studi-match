8056f1f037b0041ffc8d984da7bab80309ab459d
Jane <janeluciaschoenfeld@gmail.com>
2023-11-16 14:50:08 +0100

    delete user preferences

--- lib/screens/account/account_screen.dart
+++ lib/screens/account/account_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:studi_match/screens/bookmarks/bookmarks_screen.dart';
-import 'package:studi_match/widgets/account/user_preferences_widget.dart';
 import 'package:studi_match/widgets/router/nav_router.dart';
 
 import '../authentication/authentication_screen.dart';
@@ -133,10 +132,6 @@ class _AccountScreenState extends State<AccountScreen> {
                         ),
                       ],
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const UserPreferencesWidget(),
                     const SizedBox(
                       height: 20,
                     ),
