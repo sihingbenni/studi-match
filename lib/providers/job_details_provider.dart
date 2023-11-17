5f840d8c12876c8833690f90284ad97bdfc444f3
sihingbenni <sihing.benni@gmx.de>
2023-11-17 16:17:48 +0100

    ADD increment job detail views

--- lib/providers/job_details_provider.dart
+++ lib/providers/job_details_provider.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:studi_match/models/job.dart';
 import 'package:studi_match/models/job_details.dart';
 import 'package:studi_match/services/employment_agency/job_details_service.dart';
+import 'package:studi_match/services/firebase/job_service.dart';
 
 class JobDetailsProvider extends ChangeNotifier {
 
   late JobDetails? jobDetails;
   late Job? job;
 
+  final JobService _jobFirebaseService = JobService();
+
   void getDetails(Job job) async {
     this.job = job;
     EAJobDetailsService jobDetailsService = EAJobDetailsService();
     jobDetails = await jobDetailsService.callJobsApi(job.hashId);
+    _jobFirebaseService.incrementDetailViews(job.hashId);
     job.jobDetails = jobDetails;
     notifyListeners();
   }
