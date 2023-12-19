1f85961bd541f40076efdc569bbec2a0bb46cef1
jane <janeluciaschoenfeld@gmail.com>
2023-12-19 18:55:02 +0100

    feat: init first test

---
+++ test/screens/home_screen_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:studi_match/screens/authentication/authentication_screen.dart';
+import 'package:studi_match/screens/home/home_screen.dart';
+
+void main() {
+  testWidgets('HomeScreen widget renders correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+
+    // Verify that the widget displays the StudiMatch logo and the authentication button.
+    expect(find.text('StudiMatch'), findsOneWidget);
+    expect(find.text('Match dein nächstes Abenteuer.'), findsOneWidget);
+    expect(find.text('Jetzt durchstarten 🚀'), findsOneWidget);
+    expect(find.text('und finde dein neues Team hier!'), findsOneWidget);
+
+    // Tap the authentication button and verify that it navigates to the AuthenticationScreen.
+    await tester.tap(find.text('Jetzt durchstarten 🚀'));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(AuthenticationScreen), findsOneWidget);
+  });
+}
